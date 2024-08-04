6b11180b228370385735d58136c29add8e4be91c
lraithel15133 <lraithel15133@gmail.com>
2024-08-03 20:01:37 -0500

    Main Commits

--- activate.js
+++ activate.js
@@ -1,5 +1,3 @@
-// activate.js
-
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const navMenu = document.querySelector('nav ul');
@@ -7,13 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const contentSections = document.querySelectorAll('.content');
     const pageHeader = document.querySelector('.page-header');
 
-    // Toggle navigation for mobile
     menuToggle.addEventListener('click', function () {
         navMenu.classList.toggle('active');
-        menuToggle.classList.toggle('open'); // Toggle open class for menu toggle
+        menuToggle.classList.toggle('open'); 
     });
 
-    // Set active class based on current URL
     const setActiveLink = () => {
         const currentLocation = window.location.hash || '#explore';
         navLinks.forEach(link => {
@@ -21,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if (contentId === currentLocation) {
                 link.classList.add('active');
                 document.querySelector(contentId).style.display = 'block';
-                pageHeader.textContent = link.textContent; // Update page header text
+                pageHeader.textContent = link.textContent; 
             } else {
                 link.classList.remove('active');
                 document.querySelector(contentId).style.display = 'none';
@@ -29,13 +25,11 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     };
 
-    // Set active link on page load
     setActiveLink();
 
-    // Update active link on click
     navLinks.forEach(link => {
         link.addEventListener('click', function (e) {
-            e.preventDefault(); // Prevent default link behavior
+            e.preventDefault(); 
             window.location.hash = this.getAttribute('href');
             setActiveLink();
         });
